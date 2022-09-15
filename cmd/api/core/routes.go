cebd8c26b3ec5d352a7e96c7ece3a5697009e5c3
Yousuf Jawwad <yousuf@maher.pk>
2022-09-15 16:08:13 +0500

    chore(): cleanup

--- cmd/api/core/routes.go
+++ cmd/api/core/routes.go
@@ -1,15 +1,17 @@
 package core
 
 import (
+	"net/http"
+
 	"github.com/gocql/gocql"
 	"github.com/labstack/echo/v4"
-	"net/http"
 
 	"go.breu.io/ctrlplane/internal/db"
 	"go.breu.io/ctrlplane/internal/entities"
 	"go.breu.io/ctrlplane/internal/shared"
 )
 
+// CreateRoutes creates the routes for the app
 func CreateRoutes(g *echo.Group, middlewares ...echo.MiddlewareFunc) {
 	apps := &AppRoutes{}
 	g.POST("/apps", apps.Create)
@@ -17,13 +19,14 @@ func CreateRoutes(g *echo.Group, middlewares ...echo.MiddlewareFunc) {
 	g.GET("/apps/:slug", apps.Get)
 
 	g.POST("/apps/:slug/repos", apps.CreateAppRepo)
-	g.GET("/apps/:slug/repos", apps.GetAppRepo)
+	g.GET("/apps/:slug/repos", apps.GetAppRepos)
 }
 
 type (
 	AppRoutes struct{}
 )
 
+// Create creates a new app
 func (a *AppRoutes) Create(ctx echo.Context) error {
 	request := &AppCreateRequest{}
 	if err := ctx.Bind(request); err != nil {
@@ -39,6 +42,7 @@ func (a *AppRoutes) Create(ctx echo.Context) error {
 	return ctx.JSON(http.StatusCreated, app)
 }
 
+// Get gets an app by slug
 func (a *AppRoutes) Get(ctx echo.Context) error {
 	app := &entities.App{}
 	params := db.QueryParams{"slug": "'" + ctx.Param("slug") + "'"}
@@ -50,6 +54,7 @@ func (a *AppRoutes) Get(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, app)
 }
 
+// List lists all apps associated with the team
 func (a *AppRoutes) List(ctx echo.Context) error {
 	result := make([]entities.App, 0)
 	params := db.QueryParams{"team_id": shared.GetTeamIDFromContext(ctx)}
@@ -61,6 +66,25 @@ func (a *AppRoutes) List(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, result)
 }
 
+// GetAppRepos gets an app repos by slug
+func (a *AppRoutes) GetAppRepos(ctx echo.Context) error {
+	result := make([]entities.AppRepo, 0)
+	app := &entities.App{}
+
+	params := db.QueryParams{"slug": "'" + ctx.Param("slug") + "'", "team_id": shared.GetTeamIDFromContext(ctx)}
+	if err := db.Get(app, params); err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "app not found")
+	}
+
+	params = db.QueryParams{"app_id": app.ID.String()}
+	if err := db.Filter(&entities.AppRepo{}, &result, params); err != nil {
+		return err
+	}
+
+	return ctx.JSON(http.StatusOK, result)
+}
+
+// CreateAppRepo creates a new app repo
 func (a *AppRoutes) CreateAppRepo(ctx echo.Context) error {
 	request := &AppRepoCreateRequest{}
 	if err := ctx.Bind(request); err != nil {
@@ -79,23 +103,6 @@ func (a *AppRoutes) CreateAppRepo(ctx echo.Context) error {
 	default:
 		return echo.NewHTTPError(http.StatusInternalServerError, "unsupported git provider")
 	}
-}
-
-func (a *AppRoutes) GetAppRepo(ctx echo.Context) error {
-	result := make([]entities.AppRepo, 0)
-	app := &entities.App{}
-
-	params := db.QueryParams{"slug": "'" + ctx.Param("slug") + "'", "team_id": shared.GetTeamIDFromContext(ctx)}
-	if err := db.Get(app, params); err != nil {
-		return echo.NewHTTPError(http.StatusNotFound, "app not found")
-	}
-
-	params = db.QueryParams{"app_id": app.ID.String()}
-	if err := db.Filter(&entities.AppRepo{}, &result, params); err != nil {
-		return err
-	}
-
-	return ctx.JSON(http.StatusOK, result)
 }
 
 func (a *AppRoutes) github(ctx echo.Context, request *AppRepoCreateRequest, app *entities.App) error {
