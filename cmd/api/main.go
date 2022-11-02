e96190f907294fdb693c974756129e4b97ab0540
Yousuf Jawwad <yousuf@maher.pk>
2022-11-02 16:35:16 +0500

    chore: [sc-2512] Breu Community License 1.0 (#9)
    * chore(): removed license from cqls
    
    * chore(): removed old headers
    
    * chore(): added disclaimer text to license
    
    * chore(): updated text for source code files
    
    * chore(): updated minor adjustments
    
    * chore(): ran licensure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,24 @@
 // Copyright © 2022, Breu Inc. <info@breu.io>. All rights reserved. 
 //
-// This software is made available by Breu, Inc., under the terms of the Breu  
-// Community License Agreement, Version 1.0 located at  
-// http://www.breu.io/breu-community-license/v1. BY INSTALLING, DOWNLOADING,  
-// ACCESSING, USING OR DISTRIBUTING ANY OF THE SOFTWARE, YOU AGREE TO THE TERMS  
-// OF SUCH LICENSE AGREEMENT. 
+// This software is made available by Breu, Inc., under the terms of the 
+// Breu Community License Agreement ("BCL Agreement"), version 1.0, found at  
+// https://www.breu.io/license/community. By installating, downloading, 
+// accessing, using or distrubting any of the software, you agree to the  
+// terms of the license agreement. 
+//
+// The above copyright notice and the subsequent license agreement shall be 
+// included in all copies or substantial portions of the software. 
+//
+// Breu, Inc. HEREBY DISCLAIMS ANY AND ALL WARRANTIES AND CONDITIONS, EXPRESS, 
+// IMPLIED, STATUTORY, OR OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF 
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, WITH RESPECT TO THE 
+// SOFTWARE. 
+//
+// Breu, Inc. SHALL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND, INCLUDING BUT NOT 
+// LIMITED TO, LOST PROFITS OR ANY CONSEQUENTIAL, SPECIAL, INCIDENTAL, INDIRECT, 
+// OR DIRECT DAMAGES, HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, ARISING 
+// OUT OF THIS AGREEMENT. THE FOREGOING SHALL APPLY TO THE EXTENT PERMITTED BY  
+// APPLICABLE LAW. 
 
 package main
 
