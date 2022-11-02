bc54e7b88927872d5f6ba798e55db235f065c1db
Yousuf Jawwad <yousuf@maher.pk>
2022-11-02 15:46:30 +0500

    chore(): removed old headers

--- internal/shared/logger/zap.go
+++ internal/shared/logger/zap.go
@@ -1,11 +1,3 @@
-// Copyright © 2022, Breu Inc. <info@breu.io>. All rights reserved. 
-//
-// This software is made available by Breu, Inc., under the terms of the Breu  
-// Community License Agreement, Version 1.0 located at  
-// http://www.breu.io/breu-community-license/v1. BY INSTALLING, DOWNLOADING,  
-// ACCESSING, USING OR DISTRIBUTING ANY OF THE SOFTWARE, YOU AGREE TO THE TERMS  
-// OF SUCH LICENSE AGREEMENT. 
-
 package logger
 
 import (
