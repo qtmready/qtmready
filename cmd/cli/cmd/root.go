915e199b9965edc369b23d89acdbe5760d16d8a9
Yousuf Jawwad <yousuf@maher.pk>
2022-11-03 18:29:08 +0500

    chore: [sc-2512] Breu Community License 1.0 (#10)
    * chore(): updated LICENSE
    
    * chore(): removed license from go
    
    * chore(): updated LICENSE file
    
    * chore(): removed licences
    
    * chore(): updated licensure config
    
    * chore(): ran licensure
    
    * chore(): added pre-commit hooks
    
    * chore(): run go tests on commit

--- cmd/cli/cmd/root.go
+++ cmd/cli/cmd/root.go
@@ -1,24 +1,19 @@
-// Copyright © 2022, Breu Inc. <info@breu.io>. All rights reserved. 
+// Copyright © 2022, Breu, Inc. <info@breu.io>. All rights reserved.
 //
-// This software is made available by Breu, Inc., under the terms of the 
-// Breu Community License Agreement ("BCL Agreement"), version 1.0, found at  
-// https://www.breu.io/license/community. By installating, downloading, 
-// accessing, using or distrubting any of the software, you agree to the  
-// terms of the license agreement. 
+// This software is made available by Breu, Inc., under the terms of the BREU COMMUNITY LICENSE AGREEMENT, Version 1.0,
+// found at https://www.breu.io/license/community. BY INSTALLATING, DOWNLOADING, ACCESSING, USING OR DISTRUBTING ANY OF
+// THE SOFTWARE, YOU AGREE TO THE TERMS OF THE LICENSE AGREEMENT.
 //
-// The above copyright notice and the subsequent license agreement shall be 
-// included in all copies or substantial portions of the software. 
+// The above copyright notice and the subsequent license agreement shall be included in all copies or substantial
+// portions of the software.
 //
-// Breu, Inc. HEREBY DISCLAIMS ANY AND ALL WARRANTIES AND CONDITIONS, EXPRESS, 
-// IMPLIED, STATUTORY, OR OTHERWISE, AND SPECIFICALLY DISCLAIMS ANY WARRANTY OF 
-// MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, WITH RESPECT TO THE 
-// SOFTWARE. 
+// Breu, Inc. HEREBY DISCLAIMS ANY AND ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, AND
+// SPECIFICALLY DISCLAIMS ANY WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, WITH RESPECT TO THE
+// SOFTWARE.
 //
-// Breu, Inc. SHALL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND, INCLUDING BUT NOT 
-// LIMITED TO, LOST PROFITS OR ANY CONSEQUENTIAL, SPECIAL, INCIDENTAL, INDIRECT, 
-// OR DIRECT DAMAGES, HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, ARISING 
-// OUT OF THIS AGREEMENT. THE FOREGOING SHALL APPLY TO THE EXTENT PERMITTED BY  
-// APPLICABLE LAW. 
+// Breu, Inc. SHALL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND, INCLUDING BUT NOT LIMITED TO, LOST PROFITS OR ANY
+// CONSEQUENTIAL, SPECIAL, INCIDENTAL, INDIRECT, OR DIRECT DAMAGES, HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// ARISING OUT OF THIS AGREEMENT. THE FOREGOING SHALL APPLY TO THE EXTENT PERMITTED BY  APPLICABLE LAW.
 
 package cmd
 
@@ -36,8 +31,8 @@ var (
 		Use:   "ctrlplane",
 		Short: "ctrlplane is a multi stage release rollout engine with pre-emptive rollbacks.",
 		Long: `
-ctrlplane is a multi stage release rollout engine for cloud-native applications. It is designed to be used in 
-conjunction with a CI/CD pipeline & near realtime application monitoring to provide a safe and reliable rollout 
+ctrlplane is a multi stage release rollout engine for cloud-native applications. It is designed to be used in
+conjunction with a CI/CD pipeline & near realtime application monitoring to provide a safe and reliable rollout
 process with rollbacks for microservices.
 
 Currently, it only supports monorepo, but poly repo support is on the roadmap.
