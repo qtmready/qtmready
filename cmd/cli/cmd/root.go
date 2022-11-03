7e57c16374fef8ae2ebb4121a1f203f530e47b6c
Yousuf Jawwad <yousuf@maher.pk>
2022-11-03 17:27:54 +0500

    chore(): added pre-commit hooks

--- cmd/cli/cmd/root.go
+++ cmd/cli/cmd/root.go
@@ -31,8 +31,8 @@ var (
 		Use:   "ctrlplane",
 		Short: "ctrlplane is a multi stage release rollout engine with pre-emptive rollbacks.",
 		Long: `
-ctrlplane is a multi stage release rollout engine for cloud-native applications. It is designed to be used in 
-conjunction with a CI/CD pipeline & near realtime application monitoring to provide a safe and reliable rollout 
+ctrlplane is a multi stage release rollout engine for cloud-native applications. It is designed to be used in
+conjunction with a CI/CD pipeline & near realtime application monitoring to provide a safe and reliable rollout
 process with rollbacks for microservices.
 
 Currently, it only supports monorepo, but poly repo support is on the roadmap.
