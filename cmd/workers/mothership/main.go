24a325ad5104edb8cf31eac39d282fdab0597cdb
Umer Arshad <umer@breu.io>
2023-12-18 01:06:57 +0500

    register OnGithubActionResult with mothership

--- cmd/workers/mothership/main.go
+++ cmd/workers/mothership/main.go
@@ -56,6 +56,7 @@ func main() {
 	providerWrkr.RegisterWorkflow(ghwfs.OnPullRequestEvent)
 	providerWrkr.RegisterWorkflow(ghwfs.OnLabelEvent)
 	providerWrkr.RegisterWorkflow(ghwfs.PollMergeQueue)
+	providerWrkr.RegisterWorkflow(ghwfs.OnGithubActionResult)
 
 	// provider activities
 	providerWrkr.RegisterActivity(&github.Activities{})
