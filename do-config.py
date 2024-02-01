90feb6d7e9a665b60d1f17a0d3ee89caf5ce9dea
Umer Arshad <umer@breu.io>
2024-02-01 21:52:36 +0000

    update config file to add two repos

--- do-config.py
+++ do-config.py
@@ -1,21 +1,23 @@
 import requests
 import json
 
-stack_name = "quantm poc"
+stack_name = "quantm mutli repo poc"
 
 #Getting Access Token
 print("Logging in")
-url = "http://localhost:8000/auth/login"
+url = "https://api.breu.ngrok.io/auth/login"
 payload = {
-    "email": "mahad@breu.io",
+    "email": "umer@breu.io",
     "password": "pass123"
 }
 headers = {"Content-Type": "application/json"}
-access_token = requests.request("POST", url, json=payload, headers=headers).json()["access_token"]
+res = requests.request("POST", url, json=payload, headers=headers)
+print(res)
+access_token = res.json()["access_token"]
 
 #Creating Stack
 print("Creating Stack")
-url = "http://localhost:8000/core/stacks"
+url = "https://api.breu.ngrok.io/core/stacks"
 payload = {"name": stack_name}
 headers = {
     "Content-Type": "application/json",
@@ -25,54 +27,67 @@ stack_id = requests.request("POST", url, json=payload, headers=headers).json()["
 
 #Creating Repo
 print("Creating Repo")
-url = "http://localhost:8000/core/repos"
+url = "https://api.breu.ngrok.io/core/repos"
 payload = {
     "stack_id": stack_id,
     "provider": "github",
-    "provider_id": "684576037",
+    "provider_id": "746408294",
     "default_branch": "main",
-    "name": "cargoflo",
-    "is_monorepo": True
+    "name": "Quantm-testing-2",
+    "is_monorepo": False
 }
 repo_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
 
-#Creating Resource
-print("Creating Resource")
-url = "http://localhost:8000/core/resources"
-payloadx = {
-    "Name": "CloudRun_CargoFlo",
-    "provider": "GCP",
-    "driver": "cloudrun",
-    "stack_id": stack_id,
-    "Config": '{"properties":{"generation":"second-generation","cpu":"2000m","memory":"1024Mi"},"output":{"env":[{"url":"CloudRun_CargoFlo_URL"}]}}',
-    "immutable": True
-}
-rsid = requests.request("POST", url, json=payloadx, headers=headers).json()["id"]
-
-#creating Workload
-print("Creating Workload")
-url = "http://localhost:8000/core/workloads"
+#Creating Repo
+print("Creating Repo")
+url = "https://api.breu.ngrok.io/core/repos"
 payload = {
-    "Name": "api-quantm",
-    "kind": "worker",
-    "repo_id": repo_id,
-    "repo_path": "https://github.com/breuHQ/cargoflo",
-    "resource_id": rsid,
     "stack_id": stack_id,
-    "builder": "",
-    "container": '{"image": "europe-west3-docker.pkg.dev/cargoflo-dev-400720/cloud-run-source-deploy/cargoflo/api"}'
+    "provider": "github",
+    "provider_id": "746408201",
+    "default_branch": "main",
+    "name": "Quantm-testing-1",
+    "is_monorepo": False
 }
-workload_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
+repo_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
 
-#creating BluePrint
-print("Creating BluePrint")
-url = "http://localhost:8000/core/blueprints"
-payload = {
-    
-    "name" : "CargoFlo blueprint",
-    "stack_id" : stack_id,
-    "rollout_budget" : "300",
-    "regions" : { "gcp": ["europe-west3"], "aws": [], "azure": [], "default": [] },
-    "provider_config" : '{"project": "cargoflo-dev-400720"}'
-}
-blueprint_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
+# #Creating Resource
+# print("Creating Resource")
+# url = "https://api.breu.ngrok.io/core/resources"
+# payloadx = {
+#     "Name": "CloudRun_CargoFlo",
+#     "provider": "GCP",
+#     "driver": "cloudrun",
+#     "stack_id": stack_id,
+#     "Config": '{"properties":{"generation":"second-generation","cpu":"2000m","memory":"1024Mi"},"output":{"env":[{"url":"CloudRun_CargoFlo_URL"}]}}',
+#     "immutable": True
+# }
+# rsid = requests.request("POST", url, json=payloadx, headers=headers).json()["id"]
+
+# #creating Workload
+# print("Creating Workload")
+# url = "https://api.breu.ngrok.io/core/workloads"
+# payload = {
+#     "Name": "api-quantm",
+#     "kind": "worker",
+#     "repo_id": repo_id,
+#     "repo_path": "https://github.com/breuHQ/cargoflo",
+#     "resource_id": rsid,
+#     "stack_id": stack_id,
+#     "builder": "",
+#     "container": '{"image": "europe-west3-docker.pkg.dev/cargoflo-dev-400720/cloud-run-source-deploy/cargoflo/api"}'
+# }
+# workload_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
+
+# #creating BluePrint
+# print("Creating BluePrint")
+# url = "https://api.breu.ngrok.io/core/blueprints"
+# payload = {
+
+#     "name" : "CargoFlo blueprint",
+#     "stack_id" : stack_id,
+#     "rollout_budget" : "300",
+#     "regions" : { "gcp": ["europe-west3"], "aws": [], "azure": [], "default": [] },
+#     "provider_config" : '{"project": "cargoflo-dev-400720"}'
+# }
+# blueprint_id = requests.request("POST", url, json=payload, headers=headers).json()["id"]
