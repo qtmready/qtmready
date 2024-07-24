644b551df47f89a29cad57048fd450b2acaaa691
Yousuf Jawwad <yousuf@maher.pk>
2024-07-24 09:24:41 +0500

    chore(core): added activites at fill

--- internal/core/repo_io__states.go
+++ internal/core/repo_io__states.go
@@ -1,8 +1,11 @@
 package core
 
 import (
+	"errors"
 	"time"
 
+	"github.com/google/uuid"
+	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 
 	"go.breu.io/quantm/internal/core/timers"
@@ -11,6 +14,7 @@ import (
 // Workflow States.
 type (
 	RepoIOBranchCtrlState struct {
+		activties   *RepoActivities    // activities is the activities for the branch control state
 		repo        *Repo              // Repo is the db record of the repo
 		branch      string             // Branch is the name of the branch
 		created_at  time.Time          // created_at is the time when the branch was created
@@ -102,7 +106,7 @@ func (state *RepoIOBranchCtrlState) get_repo_data(ctx workflow.Context) *RepoIOR
 	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
 	ctx = workflow.WithActivityOptions(ctx, opts)
 
-	_ = state.run(ctx, "get_repo_data", io.GetRepoData, data, state.repo.CtrlID)
+	_ = state.do(ctx, "get_repo_data", io.GetRepoData, data, state.repo.CtrlID)
 
 	return data
 }
@@ -121,9 +125,62 @@ func (state *RepoIOBranchCtrlState) calculate_complexity(ctx workflow.Context, p
 	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
 	ctx = workflow.WithActivityOptions(ctx, opts)
 
-	_ = state.run(ctx, "calculate_complexity", Instance().RepoIO(state.repo.Provider).DetectChanges, changes, detect)
+	_ = state.do(ctx, "calculate_complexity", Instance().RepoIO(state.repo.Provider).DetectChanges, changes, detect)
 
 	return changes
+}
+
+func (state *RepoIOBranchCtrlState) create_session(ctx workflow.Context) workflow.Context {
+	opts := &workflow.SessionOptions{ExecutionTimeout: 30 * time.Minute, CreationTimeout: 60 * time.Minute}
+	ctx, _ = workflow.CreateSession(ctx, opts)
+
+	return ctx
+}
+
+// clone_at_commit clones the branch at the specified commit.
+func (state *RepoIOBranchCtrlState) clone_at_commit(ctx workflow.Context, push *RepoIOSignalPushPayload) *RepoIOClonePayload {
+	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
+	ctx = workflow.WithActivityOptions(ctx, opts)
+
+	cloned := &RepoIOClonePayload{Repo: state.repo, Push: push, Branch: state.branch}
+	_ = workflow.SideEffect(ctx, func(ctx workflow.Context) any { return "/tmp/" + uuid.New().String() }).Get(&cloned.Path)
+
+	_ = state.do(ctx, "clone_at_commit", state.activties.CloneBranch, nil, cloned)
+
+	return cloned
+}
+
+func (state *RepoIOBranchCtrlState) fetch_branch(ctx workflow.Context, cloned *RepoIOClonePayload) {
+	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
+	ctx = workflow.WithActivityOptions(ctx, opts)
+
+	_ = state.do(ctx, "fetch_branch", state.activties.FetchBranch, nil, cloned)
+}
+
+func (state *RepoIOBranchCtrlState) rebase_at_commit(ctx workflow.Context, cloned *RepoIOClonePayload) error {
+	retry_policy := &temporal.RetryPolicy{NonRetryableErrorTypes: []string{"RepoIORebaseError"}}
+	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute, RetryPolicy: retry_policy}
+	ctx = workflow.WithActivityOptions(ctx, opts)
+
+	response := &RepoIORebaseAtCommitResponse{}
+
+	if err := state.do(ctx, "rebase_at_commit", state.activties.RebaseAtCommit, response, cloned); err != nil {
+		var apperr *temporal.ApplicationError
+		if errors.As(err, &apperr) && apperr.Type() == "RepoIORebaseError" {
+			return NewRepoIORebaseError(cloned.Push.After, "fetch the commit message here")
+		}
+
+		return nil
+	}
+
+	return nil
+}
+
+func (state *RepoIOBranchCtrlState) remove_cloned(ctx workflow.Context, cloned *RepoIOClonePayload) {
+	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
+	ctx = workflow.WithActivityOptions(ctx, opts)
+
+	_ = state.do(ctx, "remove_cloned", state.activties.RemoveClonedAtPath, nil, cloned.Path)
 }
 
 // warn_complexity sends a warning message to the linked message provider if the complexity of the changes exceeds the threshold.
@@ -136,7 +193,7 @@ func (state *RepoIOBranchCtrlState) warn_complexity(ctx workflow.Context, push *
 	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
 	ctx = workflow.WithActivityOptions(ctx, opts)
 
-	_ = state.run(ctx, "warn_complexity", io.SendNumberOfLinesExceedMessage, nil, msg)
+	_ = state.do(ctx, "warn_complexity", io.SendNumberOfLinesExceedMessage, nil, msg)
 }
 
 func (state *RepoIOBranchCtrlState) warn_stale(ctx workflow.Context, data *RepoIORepoData) {
@@ -146,45 +203,26 @@ func (state *RepoIOBranchCtrlState) warn_stale(ctx workflow.Context, data *RepoI
 	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
 	ctx = workflow.WithActivityOptions(ctx, opts)
 
-	_ = state.run(ctx, "warn_stale", io.SendStaleBranchMessage, nil, msg)
-}
-
-// increment is a helper function that increments the steps counter in the RepoIOBranchCtrlState.
-func (state *RepoIOBranchCtrlState) increment(ctx workflow.Context) {
-	_ = state.mutex.Lock(ctx)
-	defer state.mutex.Unlock()
-
-	state.counter++
-}
-
-func (state *RepoIOBranchCtrlState) create_session(ctx workflow.Context) workflow.Context {
-	opts := &workflow.SessionOptions{ExecutionTimeout: 30 * time.Minute, CreationTimeout: 60 * time.Minute}
-	ctx, _ = workflow.CreateSession(ctx, opts)
-
-	return ctx
-}
-
-func (state *RepoIOBranchCtrlState) clone_at_commit(ctx workflow.Context, push *RepoIOSignalPushPayload) *RepoIOClonePayload {
-	return nil
-}
-
-func (state *RepoIOBranchCtrlState) fetch_branch(ctx workflow.Context, cloned *RepoIOClonePayload) {}
-
-func (state *RepoIOBranchCtrlState) rebase_at_commit(ctx workflow.Context, cloned *RepoIOClonePayload) error {
-	return nil
+	_ = state.do(ctx, "warn_stale", io.SendStaleBranchMessage, nil, msg)
 }
 
 func (state *RepoIOBranchCtrlState) warn_merge_conflict(ctx workflow.Context, push *RepoIOSignalPushPayload) {
+	opts := workflow.ActivityOptions{StartToCloseTimeout: time.Minute}
+	ctx = workflow.WithActivityOptions(ctx, opts)
+
+	for_user := push.User != nil && push.User.IsMessageProviderLinked
+	msg := NewMergeConflictMessage(push, state.repo, state.branch, for_user)
+	io := Instance().MessageIO(state.repo.MessageProvider)
+
+	_ = state.do(ctx, "warn_merge_conflict", io.SendMergeConflictsMessage, nil, msg)
 }
 
-func (state *RepoIOBranchCtrlState) remove_cloned(ctx workflow.Context, cloned *RepoIOClonePayload) {}
-
-// run is a helper function that executes an activity within the context of the RepoIOBranchCtrlState.
+// do is a helper function that executes an activity within the context of the RepoIOBranchCtrlState.
 // It logs the start and success of the activity, and increments the steps counter in the state.
 // If the activity fails, the function logs the error and returns it.
 //
 // NOTE: This assumes that workflow.Context has been updated with activity options.
-func (state *RepoIOBranchCtrlState) run(ctx workflow.Context, action string, activity any, result any, args ...any) error {
+func (state *RepoIOBranchCtrlState) do(ctx workflow.Context, action string, activity any, result any, args ...any) error {
 	logger := NewRepoIOWorkflowLogger(ctx, state.repo, "branch_ctrl", state.branch, action)
 	logger.Info("init")
 
@@ -200,6 +238,14 @@ func (state *RepoIOBranchCtrlState) run(ctx workflow.Context, action string, act
 	return nil
 }
 
+// increment is a helper function that increments the steps counter in the RepoIOBranchCtrlState.
+func (state *RepoIOBranchCtrlState) increment(ctx workflow.Context) {
+	_ = state.mutex.Lock(ctx)
+	defer state.mutex.Unlock()
+
+	state.counter++
+}
+
 // shutdown is called to mark the RepoIOBranchCtrlState as inactive and cancel any associated timers.
 // This function should be called when the branch control state is no longer needed, such as branch is being deleted or merged.
 func (state *RepoIOBranchCtrlState) shutdown(ctx workflow.Context) {
@@ -209,6 +255,7 @@ func (state *RepoIOBranchCtrlState) shutdown(ctx workflow.Context) {
 
 func NewBranchCtrlState(ctx workflow.Context, repo *Repo, branch string) *RepoIOBranchCtrlState {
 	return &RepoIOBranchCtrlState{
+		activties:  &RepoActivities{},
 		repo:       repo,
 		branch:     branch,
 		created_at: timers.Now(ctx),
