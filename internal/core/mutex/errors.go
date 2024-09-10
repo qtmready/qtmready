23b5eddf37ac27cce66437b5da0fe2a6eefad95b
Yousuf Jawwad <ysf@breu.io>
2024-09-10 18:31:55 +0500

    chore(): updated lincense to FSL-1.1-Apache-2.0 (#177)

--- internal/core/mutex/errors.go
+++ internal/core/mutex/errors.go
@@ -1,19 +1,19 @@
-// Copyright © 2023, Breu, Inc. <info@breu.io>. All rights reserved.
+// Copyright © 2022, 2024, Breu, Inc. <info@breu.io>
 //
-// This software is made available by Breu, Inc., under the terms of the BREU COMMUNITY LICENSE AGREEMENT, Version 1.0,
-// found at https://www.breu.io/license/community. BY INSTALLING, DOWNLOADING, ACCESSING, USING OR DISTRIBUTING ANY OF
-// THE SOFTWARE, YOU AGREE TO THE TERMS OF THE LICENSE AGREEMENT.
+// We hereby irrevocably grant you an additional license to use the Software under the Apache License, Version 2.0 that
+// is effective on the second anniversary of the date we make the Software available. On or after that date, you may use
+// the Software under the Apache License, Version 2.0, in which case the following will apply:
 //
-// The above copyright notice and the subsequent license agreement shall be included in all copies or substantial
-// portions of the software.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+// the License.
 //
-// Breu, Inc. HEREBY DISCLAIMS ANY AND ALL WARRANTIES AND CONDITIONS, EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, AND
-// SPECIFICALLY DISCLAIMS ANY WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, WITH RESPECT TO THE
-// SOFTWARE.
+// You may obtain a copy of the License at
 //
-// Breu, Inc. SHALL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND, INCLUDING BUT NOT LIMITED TO, LOST PROFITS OR ANY
-// CONSEQUENTIAL, SPECIAL, INCIDENTAL, INDIRECT, OR DIRECT DAMAGES, HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// ARISING OUT OF THIS AGREEMENT. THE FOREGOING SHALL APPLY TO THE EXTENT PERMITTED BY APPLICABLE LAW.
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
 
 package mutex
 
