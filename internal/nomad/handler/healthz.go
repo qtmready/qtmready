fa0a13869aefd81ec0d139bcb87bdb2512990e07
Yousuf Jawwad <ysf@breu.io>
2024-10-23 23:04:22 +0500

    chore(): healthcheck service

---
+++ internal/nomad/handler/healthz.go
@@ -0,0 +1,21 @@
+package handler
+
+import (
+	"context"
+
+	healthzv1 "go.breu.io/quantm/internal/nomad/proto/ctrlplane/healthz/v1"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+type (
+	HealthCheckService struct {
+		healthzv1.UnimplementedHealthCheckServiceServer
+	}
+)
+
+func (s *HealthCheckService) Status(ctx context.Context, _ *emptypb.Empty) (*healthzv1.HealthCheckResponse, error) {
+	return &healthzv1.HealthCheckResponse{
+		Database: true,
+		Temporal: true,
+	}, nil
+}
