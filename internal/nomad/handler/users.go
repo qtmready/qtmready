ac449206a3387f713cad16262eacd9bb3e25c2af
muhammad-asghar-ali <masgharali.eng@gmail.com>
2024-10-24 17:03:18 +0500

    chore(): user service and update the nomad/proto

---
+++ internal/nomad/handler/users.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"context"
+	"net/http"
+
+	"connectrpc.com/connect"
+
+	authv1 "go.breu.io/quantm/internal/nomad/proto/ctrlplane/auth/v1"
+	"go.breu.io/quantm/internal/nomad/proto/ctrlplane/auth/v1/authv1connect"
+)
+
+type (
+	UserService struct{}
+)
+
+func (s *UserService) CreateUser(
+	ctx context.Context,
+	req *connect.Request[authv1.CreateUserRequest],
+) (*connect.Response[authv1.CreateUserResponse], error) {
+	return nil, nil
+}
+
+func NewUserSericeServiceHandler() (string, http.Handler) {
+	return authv1connect.NewUserServiceHandler(&UserService{})
+}
