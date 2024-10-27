54dd0408ab8055c8fbe1d5b367ac16fc3acf2bb6
Yousuf Jawwad <ysf@breu.io>
2024-10-27 11:45:07 +0500

    chore: minor cleanup

--- internal/nomad/convert/users.go
+++ internal/nomad/convert/users.go
@@ -8,6 +8,9 @@ import (
 	authv1 "go.breu.io/quantm/internal/nomad/proto/ctrlplane/auth/v1"
 )
 
+// UserToProto converts a User entity to its protobuf representation.
+//
+// It maps all fields from the User entity to corresponding fields in the authv1.User protobuf message.
 func UserToProto(user *entities.User) *authv1.User {
 	return &authv1.User{
 		Id:         UUIDToProto(user.ID),
@@ -22,6 +25,9 @@ func UserToProto(user *entities.User) *authv1.User {
 	}
 }
 
+// ProtoToUser converts a authv1.User protobuf message to a User entity.
+//
+// It maps all fields from the authv1.User protobuf message to corresponding fields in the User entity.
 func ProtoToUser(proto *authv1.User) *entities.User {
 	return &entities.User{
 		ID:         ProtoToUUID(proto.GetId()),
@@ -36,6 +42,12 @@ func ProtoToUser(proto *authv1.User) *entities.User {
 	}
 }
 
+// ProtoToCreateUserParams converts a CreateUserRequest protobuf message to CreateUserParams.
+//
+// It maps the first name, last name, and email from the protobuf message to the corresponding fields in the
+// CreateUserParams. The password is hashed using bcrypt.DefaultCost.
+//
+// TODO: Implement actual password hashing using the provided password in the protobuf message.
 func ProtoToCreateUserParams(proto *authv1.CreateUserRequest) entities.CreateUserParams {
 	hashed, _ := bcrypt.GenerateFromPassword([]byte(""), bcrypt.DefaultCost) // TODO: hash password
 
@@ -47,6 +59,10 @@ func ProtoToCreateUserParams(proto *authv1.CreateUserRequest) entities.CreateUse
 	}
 }
 
+// ProtoToUpdateUserParams converts an UpdateUserRequest protobuf message to UpdateUserParams.
+//
+// It maps the user ID, first name, last name, email, and organization ID from the protobuf message to the corresponding
+// fields in the UpdateUserParams.
 func ProtoToUpdateUserParams(proto *authv1.UpdateUserRequest) entities.UpdateUserParams {
 	return entities.UpdateUserParams{
 		ID:        ProtoToUUID(proto.User.GetId()),
