dca47b9da3e7d1cd2cc7ab46708d365d714968a3
Yousuf Jawwad <ysf@breu.io>
2024-10-31 15:02:35 +0500

    chore(): github install service

--- internal/db/queries/users.sql
+++ internal/db/queries/users.sql
@@ -34,7 +34,8 @@ SELECT
     'updated_at', org.updated_at,
     'name', org.name,
     'domain', org.domain,
-    'slug', org.slug
+    'slug', org.slug,
+    'hooks', org.hooks
   ) AS org,
   json_agg(role.name) AS roles,
   json_agg(team.*) AS teams,
@@ -75,7 +76,8 @@ SELECT
     'updated_at', org.updated_at,
     'name', org.name,
     'domain', org.domain,
-    'slug', org.slug
+    'slug', org.slug,
+    'hooks', org.hooks
   ) AS org,
   json_agg(role.name) AS roles,
   json_agg(team.*) AS teams,
