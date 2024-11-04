b10eb18ce040b6b2694f13da8d5a0464c38419f3
Yousuf Jawwad <ysf@breu.io>
2024-11-04 22:33:30 +0500

    chore(): completing repository added event

--- internal/hooks/github/defs/webhook_payloads.go
+++ internal/hooks/github/defs/webhook_payloads.go
@@ -7,4 +7,12 @@ type (
 		Repositories []PartialRepository `json:"repositories"`
 		Sender       User                `json:"sender"`
 	}
+
+	WebhookInstallRepos struct {
+		Action              string              `json:"action"`
+		Installation        Installation        `json:"installation"`
+		RepositoriesAdeed   []PartialRepository `json:"repositories_added"`
+		RepositoriesRemoved []PartialRepository `json:"repositories_removed"`
+		RepositorySelection string              `json:"repository_selection"`
+	}
 )
