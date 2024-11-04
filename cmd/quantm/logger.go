5b4e8eaf0e8a3a36706109be86179bfdb7747352
Yousuf Jawwad <ysf@breu.io>
2024-11-04 16:51:29 +0500

    chore(): optimal logger.

--- cmd/quantm/logger.go
+++ cmd/quantm/logger.go
@@ -9,13 +9,15 @@ func configure_logger(debug bool) {
 	var handler slog.Handler
 
 	opts := &slog.HandlerOptions{
-		Level: slog.LevelInfo,
+		Level:     slog.LevelInfo,
+		AddSource: true,
 	}
 
 	handler = slog.NewJSONHandler(os.Stdout, opts)
 
 	if debug {
 		opts.Level = slog.LevelDebug
+		opts.AddSource = false
 		handler = slog.NewTextHandler(os.Stdout, opts)
 	}
 
