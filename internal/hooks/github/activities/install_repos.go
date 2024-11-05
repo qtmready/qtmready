b3ae5e469216a1444a888695e96a0a907c188020
Yousuf Jawwad <ysf@breu.io>
2024-11-05 20:56:32 +0500

    chore(): added repo

--- internal/hooks/github/activities/install_repos.go
+++ internal/hooks/github/activities/install_repos.go
@@ -2,7 +2,12 @@ package githubacts
 
 import (
 	"context"
+	"errors"
 
+	"github.com/jackc/pgx/v5"
+
+	"go.breu.io/quantm/internal/db"
+	"go.breu.io/quantm/internal/db/entities"
 	githubdefs "go.breu.io/quantm/internal/hooks/github/defs"
 )
 
@@ -10,10 +15,23 @@ type (
 	InstallRepos struct{}
 )
 
-func (a *InstallRepos) AddRepo(ctx context.Context, payload *githubdefs.SyncRepo) error {
+func (a *InstallRepos) RepoAdded(ctx context.Context, payload *githubdefs.SyncRepo) error {
 	return AddRepo(ctx, payload)
 }
 
-func (a *InstallRepos) SuspendRepo(ctx context.Context, payload *githubdefs.SyncRepo) error {
+func (a *InstallRepos) RepoRemoved(ctx context.Context, payload *githubdefs.SyncRepo) error {
 	return SuspendRepo(ctx, payload)
+}
+
+func (a *InstallRepos) GetInstallationforInstallRepos(ctx context.Context, id int64) (*entities.GithubInstallation, error) {
+	install, err := db.Queries().GetGithubInstallationByInstallationID(ctx, id)
+	if err == nil {
+		return &install, nil
+	}
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, nil
+	}
+
+	return nil, err
 }
