8960959b5b709ca46bdca7055159d4a0b200853e
alyfinder <asgr@breu.io>
2024-11-11 17:20:48 +0500

    chore(): trying to handle push event

--- internal/hooks/github/defs/activity_payloads.go
+++ internal/hooks/github/defs/activity_payloads.go
@@ -2,6 +2,8 @@ package githubdefs
 
 import (
 	"github.com/google/uuid"
+
+	"go.breu.io/quantm/internal/events"
 )
 
 type (
@@ -10,5 +12,14 @@ type (
 		Repo           PartialRepository `json:"repo"`
 		IsDeleted      bool              `json:"is_deleted"`
 		OrgID          uuid.UUID         `json:"org_id"`
+	}
+
+	RepoEventPayload struct {
+		RepoID         int64
+		InstallationID int64
+		Action         events.EventAction
+		Scope          events.EventScope
+
+		// TODO - may add the senderID for user.
 	}
 )
