22fb7f591c69b774590671a44dc40d091f5ffa2e
Yousuf Jawwad <ysf@breu.io>
2025-01-08 23:11:42 +0500

    chore(): flake now builds

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
-    # breu-go.url = "github:breuhq/flake-go";
-    breu-go.url = "/Users/jay/Work/breu/flake-go";
+    breu-go.url = "github:breuhq/flake-go";
+    # breu-go.url = "/Users/jay/Work/breu/flake-go";
   };
 
   outputs = {
@@ -25,11 +25,20 @@
         # Add libgit2 to the base environment
         base = pkgs'.setup.base [
           pkgs.openssl
+          pkgs.http-parser
+          pkgs.zlib
+          pkgs.python3 # requird for http-parser
           pkgs.libgit2
         ];
 
+        env = {};
+
+        dev = [
+          pkgs.gopls
+        ];
+
         # Set up the development shell with our base packages
-        shell = pkgs'.setup.shell base [];
+        shell = pkgs'.setup.shell base dev env;
       in {
         devShells.default = shell;
         # packages.quantm = quantm;
