eba54f9eca12ba57461613ea59d6941988704d1a
Yousuf Jawwad <ysf@breu.io>
2025-01-10 16:26:17 +0500

    chore(): supporting more base package in nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     breu-go.url = "github:breuhq/flake-go";
-    # breu-go.url = "/Users/jay/Work/breu/flake-go";
   };
 
   outputs = {
@@ -22,7 +21,7 @@
         # Apply the breu-go overlay
         pkgs' = pkgs.extend (final: prev: breu-go.overlay.${system} final prev);
 
-        # Add libgit2 to the base environment
+        # Add required dependencies to the base environment
         base = pkgs'.setup.base [
           pkgs.openssl
           pkgs.http-parser
@@ -31,17 +30,29 @@
           pkgs.libgit2
         ];
 
-        env = {};
-
-        dev = [
-          pkgs.gopls
-        ];
-
         # Set up the development shell with our base packages
-        shell = pkgs'.setup.shell base dev env;
+        shell = pkgs'.setup.shell base [] {};
+
+        quantm = pkgs.stdenv.mkDerivation {
+          name = "quantm";
+          version = "0.1.0"; # Adjust as needed
+          src = ./.; # Use the directory containing your Go code
+
+          nativeBuildInputs = base;
+
+          buildPhase = ''
+            export GOROOT="${pkgs.go_1_23}/share/go"
+            go build -x -tags static,system_libgit2 -o $out/bin/quantm ./cmd/quantm
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $out/bin/quantm $out/bin/quantm
+          '';
+        };
       in {
         devShells.default = shell;
-        # packages.quantm = quantm;
+        packages.quantm = quantm;
       }
     );
 }
